06b30deede921826ed68141cdfd6610dd86f1777
Amit Kumar Murmu <amitkumar655921@gmail.com>
2025-09-06 07:20:34 +0000

    add vite-plugin-remove-console and update configuration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { Provider } from "react-redux";
 import store from './redux/store.js';
 import AllRoutes from './AllRoutes.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <AllRoutes />
+      <BrowserRouter>
+        <AllRoutes />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
